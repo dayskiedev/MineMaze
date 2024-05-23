c5b12b47083c13fd13cfad4262b900b5d26dc8fe
SidZn <s4042149@rmit.student.edu.au>
2024-05-23 11:15:25 +1000

    Maze.h

---
+++ StarterCode/Maze.h
@@ -0,0 +1,23 @@
+#ifndef ASSIGN_MAZE_H
+#define ASSIGN_MAZE_H
+
+#include <mcpp/mcpp.h>
+
+class Maze
+{
+
+public:
+    Maze(mcpp::Coordinate basePoint, unsigned int xlen, 
+                                        unsigned int zlen,
+                                        bool mode);
+    ~Maze();
+
+private:
+    /* data */
+
+};
+
+
+
+
+#endif //ASSIGN_MAZE_H
