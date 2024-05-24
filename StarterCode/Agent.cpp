cf80c5b0b525a1ac8256a927ba926989eb436dca
SidZn <s4042149@rmit.student.edu.au>
2024-05-24 14:16:44 +1000

    MazeSolveLogic

--- StarterCode/Agent.cpp
+++ StarterCode/Agent.cpp
@@ -7,3 +7,19 @@ Agent::Agent(mcpp::Coordinate startLoc)
 Agent::~Agent()
 {
 }
+
+Left-Hand follow algorithm
+
+player coordinates = (x, y, z)
+// Considering player is facing forward in these coordinates
+
+if (x + 1 == Air)
+
+// if (left is empty)
+//  go left
+// else if (left is not empty, but straight is)
+//  go straight
+// else if (left and straight is not empty)
+//  go right 
+// else if (left, right and straight is not empty)
+// turn 180* 
