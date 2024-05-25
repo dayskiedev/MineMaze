eb30b89dcc052aadc3e8a0d8c97f376a82ed3f4d
darcyplatt <s3657395@student.rmit.edu.au>
2024-05-25 18:15:26 +1000

    implementing initial functions for maze.h/maze.cpp

--- StarterCode/Maze.cpp
+++ StarterCode/Maze.cpp
@@ -1,11 +1,38 @@
 #include "Maze.h"
 #include <random>
+#include <string>
+#include <sstream>
+#include <iostream>
 
-
-Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen, 
+Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen,
                                     unsigned int zlen,
                                     bool mode)
 {
+    this->basePoint=basePoint;
+    this->length=xlen;
+    this->width=zlen;
+    this->mode=mode;
+}
+
+std::vector<std::vector<char>> Maze::readMaze(const std::string& mazeStr)
+{
+    std::vector<std::vector<char>> mazeChars;
+    std::istringstream iSS(mazeStr);
+    std::string line;
+    while(std::getline(iSS, line)){
+        mazeChars.emplace_back();
+        for(char c : line){
+            if(c != '/n'){
+                mazeChars.back().push_back(c);
+            }
+        }
+    }
+    return mazeChars;
+}
+
+void Maze::buildMaze(std::vector<std::vector<char>>& mazeChars)
+{
+    std::vector<std::vector<mcpp::MinecraftConnection>> mazeBlocks;
 }
 
 Maze::~Maze()
--- StarterCode/Maze.h
+++ StarterCode/Maze.h
@@ -2,19 +2,35 @@
 #define ASSIGN_MAZE_H
 
 #include <mcpp/mcpp.h>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 class Maze
 {
 
 public:
-    Maze(mcpp::Coordinate basePoint, unsigned int xlen, 
+    Maze(mcpp::Coordinate basePoint, unsigned int xlen,
                                         unsigned int zlen,
                                         bool mode);
     ~Maze();
 
-private:
-    /* data */
+    /**
+     * void Maze::flattenTerrain(); TODO?? might need to implement this function
+     * but also could also be implemented in maze constructor?
+     */
 
+    std::vector<std::vector<char>> Maze::readMaze(const std::string& mazeStr);
+
+    void Maze::buildMaze(std::vector<std::vector<char>>& mazeChars);
+
+    ~Maze();
+
+private:
+    mcpp::Coordinate basePoint;
+    unsigned int length;
+    unsigned int width;
+    bool mode;
 };
 
 
