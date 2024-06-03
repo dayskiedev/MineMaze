cdf113d5ea2cdce4fa63a3c79fa94eedcb22b324
darcyplatt <s3657395@student.rmit.edu.au>
2024-06-03 12:40:04 +1000

    pre refactor for maze.h/.cpp - syncing changes

--- StarterCode/Maze.cpp
+++ StarterCode/Maze.cpp
@@ -8,13 +8,35 @@ Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen,
                                     unsigned int zlen,
                                     bool mode)
 {
-    this->basePoint=basePoint;
-    this->length=xlen;
-    this->width=zlen;
+/*    this->basePoint=basePoint;
+    this->length=xlen; //TODO remove this var and just pass it into the coordinate vectors
+    this->width=zlen; //TODO remove this var and just pass it into the coordinate vectors
     this->mode=mode;
+*/
 }
 
+std::vector<std::vector<char>> Maze::readMaze(const std::string& mazeStr)
+{
+    std::vector<std::vector<char>> mazeChars;
+    std::istringstream iSS(mazeStr);
+    std::string line;
+    while(std::getline(iSS, line)){
+        mazeChars.emplace_back();
+        for(char c : line){
+            if(c != '/n'){
+                mazeChars.back().push_back(c);
+            }
+        }
+    }
+    return mazeChars;
+}
+
+void Maze::buildMaze(std::vector<std::vector<char>>& mazeChars)
+{
+    std::vector<std::vector<mcpp::MinecraftConnection>> mazeBlocks;
+}
 
 Maze::~Maze()
 {
 }
+*/
--- StarterCode/Maze.h
+++ StarterCode/Maze.h
@@ -13,21 +13,33 @@ public:
     Maze(mcpp::Coordinate basePoint, unsigned int xlen,
                                         unsigned int zlen,
                                         bool mode);
+    ~Maze();
+
     /**
      * void Maze::flattenTerrain(); TODO?? might need to implement this function
      * but also could also be implemented in maze constructor?
      */
 
-    // header file doesnt need Maze:: before functions as this is where the function is created not where they're referenced/called -B
-    // cant use vectors
-    ~Maze();
+    std::vector<std::vector<char>> Maze::readMaze(const std::string& mazeStr);
 
+    void Maze::buildMaze(std::vector<std::vector<char>>& mazeChars);
+
+    ~Maze();
+*/
 private:
+/*   mcpp::MinecraftConnection maze;
     mcpp::Coordinate basePoint;
     unsigned int length;
     unsigned int width;
     bool mode;
+    std::vector<mcpp::Coordinate*> terrainCoordinates;
+    std::vector<mcpp::BlockType*> terrainBlocks;
+    std::vector<std::vector<mcpp::Coordinate*>> mazeCoordinates;
+    std::vector<std::vector<mcpp::BlockType*>> MazeBlocks;
+*/
 };
+
+
 
 
 #endif //ASSIGN_MAZE_H
