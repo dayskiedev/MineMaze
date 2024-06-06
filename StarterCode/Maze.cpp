183e30d20d96e7285b094c56747c3d18048ac7a4
darcyplatt <s3657395@student.rmit.edu.au>
2024-06-06 10:57:34 +1000

    flattenTerrain() fixed but not tested

--- StarterCode/Maze.cpp
+++ StarterCode/Maze.cpp
@@ -1,22 +1,22 @@
 #include "Maze.h"
 #include <random>
-//#include <string>
-//#include <sstream>
-//#include <iostream>
+// #include <string>
+// #include <sstream>
+// #include <iostream>
 
 Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen,
-                                    unsigned int zlen,
-                                    bool mode)
+           unsigned int zlen,
+           bool mode)
 {
-this->basePoint=basePoint;
-this->length=xlen;
-this->width=zlen;
-this->mode=mode;
+    this->basePoint = basePoint;
+    this->length = xlen;
+    this->width = zlen;
+    this->mode = mode;
 }
 
-void Maze::coordPushBack(mcpp::Coordinate* coordinate)
+void Maze::coordPushBack(mcpp::Coordinate *coordinate)
 {
-    MazeCoord* newCoord = new MazeCoord();
+    MazeCoord *newCoord = new MazeCoord();
     newCoord->coord = coordinate;
     newCoord->next = headCoord;
     headCoord = newCoord;
@@ -24,12 +24,11 @@ void Maze::coordPushBack(mcpp::Coordinate* coordinate)
 
 void Maze::coordPopBack()
 {
-
 }
 
-void Maze::blockPushBack(mcpp::BlockType* block)
+void Maze::blockPushBack(mcpp::BlockType *block)
 {
-    MazeBlock* newBlock = new MazeBlock();
+    MazeBlock *newBlock = new MazeBlock();
     newBlock->block = block;
     newBlock->next = headBlock;
     headBlock = newBlock;
@@ -37,40 +36,54 @@ void Maze::blockPushBack(mcpp::BlockType* block)
 
 void Maze::blockPopBack()
 {
-
 }
 
+int Maze::mazeSize() const
+{
+}
+
+// TODO might need to have getBlockCoord in this part as well to keep track of what blocktype was before changing to air --
+//  still unsure if deleting memory will reverse a block at coordinate to its orgianal state
 void Maze::flattenTerrain()
 {
-    mcpp::Coordinate* cornerFromBase = new mcpp::Coordinate((basePoint.x + length + 1), basePoint.y, (basePoint.z + width  + 1));
+    mcpp::Coordinate *cornerFromBase = new mcpp::Coordinate((basePoint.x + length + 1), basePoint.y, (basePoint.z + width + 1));
     int axisIndex_z = 0;
-    for(size_t i = 0; i <  mc.getHeights(basePoint, *cornerFromBase).size(); ++i) {
+    for (size_t i = 0; i < mc.getHeights(basePoint, *cornerFromBase).size(); ++i)
+    {
         int axisIndex_x = 0;
-        for(size_t j = 0; j < mc.getHeights(basePoint, *cornerFromBase)[i].size(); ++j) {
-                
-                if(mc.getHeights(basePoint, *cornerFromBase)[i][j] < basePoint.y) {
-                        int level_yAxis = 1;
-                        mcpp::Coordinate* getBlockCoord = new mcpp::Coordinate((basePoint.x + axisIndex_x)
-                                                        + mc.getHeights(basePoint, *cornerFromBase)[i][j],(basePoint.z + axisIndex_z));
+        for (size_t j = 0; j < mc.getHeights(basePoint, *cornerFromBase)[i].size(); ++j)
+        {
 
-                        while(mc.getHeights(basePoint, *cornerFromBase)[i][j] < basePoint.y) { //TODO this could be an infinite loop 
-                            mcpp::Coordinate* coordinate = new mcpp::Coordinate((basePoint.x + axisIndex_x),
-                                                        (mc.getHeights(basePoint, *cornerFromBase)[i][j] + level_yAxis), (basePoint.z + axisIndex_z));
-                            this->coordPushBack(coordinate);
-                            mc.setBlock(*coordinate, mc.getBlock(*getBlockCoord));
-                            mcpp::BlockType* block = new mcpp::BlockType(mc.getBlock(*coordinate));
-                            this->blockPushBack(block);
-                            ++level_yAxis;
-                        }
+            if (mc.getHeights(basePoint, *cornerFromBase)[i][j] < basePoint.y)
+            {
+                int yAxis_diff = basePoint.y - mc.getHeights(basePoint, *cornerFromBase)[i][j];
+                int level_yAxis = 1;
+                mcpp::Coordinate *getBlockCoord = new mcpp::Coordinate((basePoint.x + axisIndex_x) + mc.getHeights(basePoint, *cornerFromBase)[i][j], (basePoint.z + axisIndex_z));
+
+                while (level_yAxis != yAxis_diff)
+                {
+                    mcpp::Coordinate *coordinate = new mcpp::Coordinate((basePoint.x + axisIndex_x),
+                                                                        (mc.getHeights(basePoint, *cornerFromBase)[i][j] + level_yAxis), (basePoint.z + axisIndex_z));
+                    this->coordPushBack(coordinate);
+                    mc.setBlock(*coordinate, mc.getBlock(*getBlockCoord));
+                    mcpp::BlockType *block = new mcpp::BlockType(mc.getBlock(*coordinate));
+                    this->blockPushBack(block);
+                    ++level_yAxis;
                 }
-                else if(mc.getHeights(basePoint, *cornerFromBase)[i][j] > basePoint.y) { //TODO this could be an infinite loop 
-                        int level_yAxis = mc.getHeights(basePoint, *cornerFromBase)[i][j] - basePoint.y;
-                        //TODO might need to have getBlockCoord in this part as well to keep track of what blocktype was before changing to air --
-                        // still unsure if deleting memory will reverse a block at coordinate to its orgianal state
-                        while(mc.getHeights(basePoint, *cornerFromBase)[i][j] > basePoint.y) {
-                            --level_yAxis;
-                        }
+            }
+            else if (mc.getHeights(basePoint, *cornerFromBase)[i][j] > basePoint.y)
+            {
+                int yAxis_diff = mc.getHeights(basePoint, *cornerFromBase)[i][j] - basePoint.y;
+                while (yAxis_diff != 0)
+                {
+                    mcpp::Coordinate *coordinate = new mcpp::Coordinate((basePoint.x + axisIndex_x),
+                                                                        (mc.getHeights(basePoint, *cornerFromBase)[i][j] + yAxis_diff), (basePoint.z + axisIndex_z));
+                    this->coordPushBack(coordinate);
+                    mc.setBlock(*coordinate, mcpp::Blocks::AIR);
+                    mcpp::BlockType *block = new mcpp::BlockType(mcpp::Blocks::AIR);
+                    --yAxis_diff;
                 }
+            }
             ++axisIndex_x;
         }
         ++axisIndex_z;
@@ -78,12 +91,10 @@ void Maze::flattenTerrain()
     delete cornerFromBase;
 }
 
-void Maze::buildMaze(char** mazeStructure)
+void Maze::buildMaze(char **mazeStructure)
 {
-
 }
 
 Maze::~Maze()
 {
-
 }
--- StarterCode/Maze.h
+++ StarterCode/Maze.h
@@ -2,53 +2,52 @@
 #define ASSIGN_MAZE_H
 
 #include <mcpp/mcpp.h>
-//#include <string>
-//#include <sstream>
-//#include <iostream>
+// #include <string>
+// #include <sstream>
+// #include <iostream>
 
 class MazeCoord
 {
 public:
-    //MazeCoord(mcpp::Coordinate* coord);
-    mcpp::Coordinate* coord;
-    MazeCoord* next;
+    // MazeCoord(mcpp::Coordinate* coord);
+    mcpp::Coordinate *coord;
+    MazeCoord *next;
 };
 
 class MazeBlock
 {
 public:
-    //MazeBlock(mcpp::BlockType block);
-    mcpp::BlockType* block;
-    MazeBlock* next;
+    // MazeBlock(mcpp::BlockType block);
+    mcpp::BlockType *block;
+    MazeBlock *next;
 };
 
 class Maze
 {
 public:
     Maze(mcpp::Coordinate basePoint, unsigned int xlen,
-                                        unsigned int zlen,
-                                        bool mode);
+         unsigned int zlen,
+         bool mode);
     void flattenTerrain();
 
-    void buildMaze(char** mazeStructure);
+    void buildMaze(char **mazeStructure);
 
     ~Maze();
 
 private:
     mcpp::MinecraftConnection mc;
     mcpp::Coordinate basePoint;
-    MazeCoord* headCoord;
-    MazeBlock* headBlock;
+    MazeCoord *headCoord;
+    MazeBlock *headBlock;
     unsigned int length;
     unsigned int width;
     bool mode;
-    
-    void coordPushBack(mcpp::Coordinate* coord);
-    void coordPopBack();
-    void blockPushBack(mcpp::BlockType* block);
-    void blockPopBack();
 
-    
+    void coordPushBack(mcpp::Coordinate *coord);
+    void coordPopBack();
+    void blockPushBack(mcpp::BlockType *block);
+    void blockPopBack();
+    int mazeSize() const;
 };
 
-#endif //ASSIGN_MAZE_H
+#endif // ASSIGN_MAZE_H
